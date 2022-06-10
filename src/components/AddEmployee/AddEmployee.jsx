9ae2cb519d3bc120aed47f0ffcf44b5d28821a45
Mark Lawson <mark@walkonmars.co.uk>
2022-06-10 15:31:41 +0100

    added buttons to form

--- src/components/AddEmployee/AddEmployee.jsx
+++ src/components/AddEmployee/AddEmployee.jsx
@@ -1,10 +1,23 @@
 import React from "react";
 import "./AddEmployee.scss";
 
-const AddEmployee = ({toggleForm}) => {
+const AddEmployee = ({toggleForm, teamMember}) => {
+  const handleSubmit = (e) => {
+    e.prevent.default();
+
+    const newMember = {
+      id: teamMember[teamMember.length - 1].id + 1,
+      name: e.target.name.value,
+      role: e.target.role.value,
+    };
+  };
+
   return (
     <div>
       <form action="" className="employee-form">
+        <button className="employee-form__cancel-btn" onClick={toggleForm}>
+          Cancel
+        </button>
         <label htmlFor="name" name="name">
           Name:
         </label>
@@ -14,7 +27,7 @@ const AddEmployee = ({toggleForm}) => {
           Role:
         </label>
         <input type="text" name="role" className="employee-form__input" placeholder="Enter Job Title" />
-        <input type="submit" value="Submit" onClick={toggleForm} />
+        <input type="submit" value="Submit" className="employee-form__submit" />
       </form>
     </div>
   );
